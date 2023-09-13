55facec802c0ee8f3f59c0689702a82ee79aeaaa
rebalelaawar <rebalelaawar@gmail.com>
2023-09-13 02:17:03 -0400

    looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+function writeCards(name,event){
+ const cards = [];
+
+ for (let i = 0; i < name.length; i++){
+
+  const message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+
+  cards.push(message);
+ }
+  return cards;
+}
+
+function countDown(startingNumber){
+
+  let i = startingNumber
+  while (i >= 0)
+  {console.log (i); i--}
+}
